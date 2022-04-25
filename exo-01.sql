1f568239c2cbdafb5e8e3137ffead3047cb170e9
jibundeyare <contact@jibundeyare.com>
2022-04-25 16:14:00 +0200

    Amélioration

---
+++ exo-01.sql
@@ -0,0 +1,38 @@
+-- Exo 1
+-- Requêtes simples en lecture
+
+-- Code 1.1
+-- Voici la requête qui permet de lister tous les tags
+SELECT *
+FROM tag;
+
+-- Exo 1.1
+-- Écrivez la requête qui permet de lister tous les students
+
+-- Exo 1.2
+-- Écrivez la requête qui permet de lister tous les projects
+
+-- Exo 1.3
+-- Écrivez la requête qui permet de lister le student dont l'id est `2`
+
+-- Exo 1.4
+-- Écrivez la requête qui permet de lister les students dont l'id n'est pas `2`
+
+-- Exo 1.5
+-- Écrivez la requête qui permet de lister le project dont l'id est `3`
+
+-- Exo 1.6
+-- Écrivez la requête qui permet de lister les projects dont l'id n'est pas `3`
+
+-- Exo 1.7
+-- Écrivez la requête qui permet de lister les students dont l'email contient la chaîne de caractères `.com`
+
+-- Exo 1.8
+-- Écrivez la requête qui permet de lister les students dont la date de création est antérieure au 10 janvier 2021 inclus
+
+-- Exo 1.9
+-- Écrivez la requête qui permet de lister les projets dont le nom du client ne contient pas la chaîne de caractères `Dolores`
+
+-- Exo 1.10
+-- Écrivez la requête qui permet de lister les projets dont la date de création est postérieure au 01 juillet 2021 inclus
+
